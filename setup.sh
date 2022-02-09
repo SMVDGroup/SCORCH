1ae5e57f7890a9e43d39947400dc9d90234d8bd7
miles-mcgibbon <s1738716@ed.ac.uk>
2022-02-09 12:33:12 +0000

    Added mac os compatibility to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 if ! command -v conda &> /dev/null
 then
-    mkdir utils/miniconda3
-    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O utils/miniconda3/miniconda.sh
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    	mkdir utils/miniconda3
+    	wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O utils/miniconda3/miniconda.sh
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+	mkdir utils/miniconda3
+        curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o utils/miniconda3/miniconda.sh
     bash utils/miniconda3/miniconda.sh -b -u -p utils/miniconda3
     rm -rf utils/miniconda3/miniconda.sh
     utils/miniconda3/bin/conda init bash
@@ -9,6 +13,7 @@ then
     source ~/.bashrc
     conda deactivate
     conda config --set auto_activate_base false
+    fi
 fi
 conda config --set channel_priority strict
 conda env create -f scorch.yml python=3.6.9
