22d04a056ff3012239cbdb21f1f717003b223107
miles-mcgibbon <s1738716@ed.ac.uk>
2022-02-11 14:34:26 +0000

    Added working gwovina build instructions

--- setup.sh
+++ setup.sh
@@ -4,13 +4,11 @@ if ! command -v conda &> /dev/null
 then
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     	sudo mkdir utils/miniconda3
-      PLATFORM='linux'
     	sudo wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O utils/miniconda3/miniconda.sh
     elif [[ "$OSTYPE" == "darwin"* ]]; then
 	    brew install libomp
 	    sudo mkdir utils/miniconda3
       sudo curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o utils/miniconda3/miniconda.sh
-      PLATFORM='mac'
     fi
     echo "bash utils etc..."
     sudo bash utils/miniconda3/miniconda.sh -b -u -p utils/miniconda3
@@ -33,6 +31,14 @@ sudo chown -R $USER ~/.conda
 conda env create -f scorch.yml python=3.6.9
 
 # setup GWOVina
-tar -xzvf utils/gwovina-1.0.tar.gz
-cd utils/gwovina-1.0/$PLATFORM/release && sudo make
+echo "Unpacking GWOVina 1.0"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    PLATFORM="linux"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    PLATFORM="mac"
+fi
+cd utils && tar -xzvf gwovina-1.0.tar.gz
+cd gwovina-1.0/build/$PLATFORM/release
+echo "Building GWOVina 1.0"
+sudo make -j2
 cd --
