e1ae270cb3f9e056a0ebda0ab0f26a01c17e6228
miles-mcgibbon <milesmcgibbon@gmx.us>
2022-06-26 16:48:08 +0100

    Add code to parse new binana output dict

--- scorch.py
+++ scorch.py
@@ -72,12 +72,169 @@ def run_binana(lig, rec):
     # descriptor features as a DataFrame      #
     ###########################################
 
-    output = binana.Binana(lig, rec).out
+    binana_features = dict()
+
+    main_binana_out = binana.Binana(lig, rec).out
 
     from pprint import pprint
 
-    pprint(output)
+    pprint(main_binana_out)
+    
 
+    keep_closest_contacts = ["2.5 (HD, OA)", 
+                             "2.5 (HD, HD)", 
+                             "2.5 (HD, N)", 
+                             "2.5 (C, HD)", 
+                             "2.5 (OA, ZN)", 
+                             "2.5 (HD, ZN)", 
+                             "2.5 (A, HD)"]
+    
+    keep_close_contacts = ["4.0 (C, C)", 
+                           "4.0 (HD, OA)", 
+                           "4.0 (C, HD)", 
+                           "4.0 (C, N)", 
+                           "4.0 (A, C)",
+                           "4.0 (A, OA)", 
+                           "4.0 (N, OA)", 
+                           "4.0 (A, N)", 
+                           "4.0 (HD, N)", 
+                           "4.0 (HD, HD)", 
+                           "4.0 (A, HD)", 
+                           "4.0 (OA, OA)", 
+                           "4.0 (C, OA)", 
+                           "4.0 (N, N)",
+                           "4.0 (C, SA)", 
+                           "4.0 (HD, SA)", 
+                           "4.0 (OA, SA)", 
+                           "4.0 (N, SA)", 
+                           "4.0 (A, A)", 
+                           "4.0 (HD, S)", 
+                           "4.0 (S, ZN)", 
+                           "4.0 (N, ZN)", 
+                           "4.0 (HD, ZN)", 
+                           "4.0 (A, SA)", 
+                           "4.0 (OA, ZN)", 
+                           "4.0 (C, ZN)", 
+                           "4.0 (C, NA)", 
+                           "4.0 (NA, OA)", 
+                           "4.0 (HD, NA)", 
+                           "4.0 (N, NA)", 
+                           "4.0 (A, NA)", 
+                           "4.0 (BR, C)", 
+                           "4.0 (HD, P)", 
+                           "4.0 (F, N)", 
+                           "4.0 (F, HD)", 
+                           "4.0 (C, CL)", 
+                           "4.0 (CL, HD)"]
+
+    keep_ligand_atoms = ["LA N",
+                         "LA HD"]
+    
+    keep_elsums = [ "ElSum (C, C)",
+                    "ElSum (HD, OA)",
+                    "ElSum (C, HD)",
+                    "ElSum (C, N)",
+                    "ElSum (A, C)",
+                    "ElSum (A, OA)",
+                    "ElSum (N, OA)",
+                    "ElSum (A, N)",
+                    "ElSum (HD, HD)",
+                    "ElSum (A, HD)",
+                    "ElSum (OA, OA)",
+                    "ElSum (C, OA)",
+                    "ElSum (N, N)",
+                    "ElSum (C, SA)",
+                    "ElSum (HD, SA)",
+                    "ElSum (OA, SA)",
+                    "ElSum (N, SA)",
+                    "ElSum (A, A)",
+                    "ElSum (N, S)",
+                    "ElSum (HD, S)",
+                    "ElSum (OA, S)",
+                    "ElSum (A, SA)",
+                    "ElSum (C, NA)",
+                    "ElSum (NA, OA)",
+                    "ElSum (HD, NA)",
+                    "ElSum (N, NA)",
+                    "ElSum (A, NA)",
+                    "ElSum (BR, C)",
+                    "ElSum (HD, P)",
+                    "ElSum (OA, P)",
+                    "ElSum (N, P)",
+                    "ElSum (C, F)",
+                    "ElSum (F, N)",
+                    "ElSum (A, F)",
+                    "ElSum (CL, OA)",
+                    "ElSum (C, CL)",
+                    "ElSum (CL, N)",
+                    "ElSum (A, CL)"]
+
+    # add closest contacts to dict
+    for contact in keep_closest_contacts:
+        binana_name = contact.split('(')[-1].split(')')[0].replace(', ','_')
+        binana_features[contact] = main_binana_out['closest'].get(binana_name)
+    
+    # add close contacts to dict
+    for contact in keep_close_contacts:
+        binana_name = contact.split('(')[-1].split(')')[0].replace(', ','_')
+        binana_features[contact] = main_binana_out['close'].get(binana_name)
+    
+    # add ligand atoms to dict
+    for atom in keep_ligand_atoms:
+        binana_name = atom.split()[-1]
+        if main_binana_out['ligand_atoms'].get(binana_name) is None:
+            binana_features[atom] = 0
+        else:
+            binana_features[atom] = 1
+    
+    for elsum in keep_elsums:
+        binana_name = elsum.split('(')[-1].split(')')[0].replace(', ','_')
+        binana_features[elsum] = main_binana_out['elsums'].get(binana_name)
+
+    binana_features["BPF ALPHA SIDECHAIN"] = main_binana_out['bpfs'].get("SIDECHAIN_ALPHA")
+    binana_features["BPF ALPHA BACKBONE"] = main_binana_out['bpfs'].get("BACKBONE_ALPHA")
+    binana_features["BPF BETA SIDECHAIN"] = main_binana_out['bpfs'].get("SIDECHAIN_BETA")
+    binana_features["BPF BETA BACKBONE"] = main_binana_out['bpfs'].get("BACKBONE_BETA")
+    binana_features["BPF OTHER SIDECHAIN"] = main_binana_out['bpfs'].get("SIDECHAIN_OTHER")
+    binana_features["BPF OTHER BACKBONE"] = main_binana_out['bpfs'].get("BACKBONE_OTHER")
+
+
+    binana_features["HC ALPHA SIDECHAIN"] = main_binana_out['hydrophobics'].get("SIDECHAIN_ALPHA")
+    binana_features["HC ALPHA BACKBONE"] = main_binana_out['hydrophobics'].get("BACKBONE_ALPHA")
+    binana_features["HC BETA SIDECHAIN"] = main_binana_out['hydrophobics'].get("SIDECHAIN_BETA")
+    binana_features["HC BETA BACKBONE"] = main_binana_out['hydrophobics'].get("BACKBONE_BETA")
+    binana_features["HC OTHER SIDECHAIN"] = main_binana_out['hydrophobics'].get("SIDECHAIN_OTHER")
+    binana_features["HC OTHER BACKBONE"] = main_binana_out['hydrophobics'].get("BACKBONE_OTHER")
+
+    binana_features["HB ALPHA SIDECHAIN LIGAND"] = main_binana_out['hbonds'].get("HDONOR_LIGAND_SIDECHAIN_ALPHA")
+    binana_features["HB BETA SIDECHAIN LIGAND"] = main_binana_out['hbonds'].get("HDONOR_LIGAND_SIDECHAIN_BETA")
+    binana_features["HB BETA BACKBONE LIGAND"] = main_binana_out['hbonds'].get("HDONOR_LIGAND_BACKBONE_BETA")
+    binana_features["HB OTHER SIDECHAIN LIGAND"] = main_binana_out['hbonds'].get("HDONOR_LIGAND_SIDECHAIN_OTHER")
+    binana_features["HB OTHER BACKBONE LIGAND"] = main_binana_out['hbonds'].get("HDONOR_LIGAND_BACKBONE_OTHER")
+    binana_features["HB ALPHA SIDECHAIN RECEPTOR"] = main_binana_out['hbonds'].get("HDONOR_RECEPTOR_SIDECHAIN_ALPHA")
+    binana_features["HB ALPHA BACKBONE RECEPTOR"] = main_binana_out['hbonds'].get("HDONOR_RECEPTOR_BACKBONE_ALPHA")
+    binana_features["HB BETA SIDECHAIN RECEPTOR"] = main_binana_out['hbonds'].get("HDONOR_RECEPTOR_SIDECHAIN_BETA")
+    binana_features["HB BETA BACKBONE RECEPTOR"] = main_binana_out['hbonds'].get("HDONOR_RECEPTOR_BACKBONE_BETA")
+    binana_features["HB OTHER SIDECHAIN RECEPTOR"] = main_binana_out['hbonds'].get("HDONOR_RECEPTOR_SIDECHAIN_OTHER")
+    binana_features["HB OTHER BACKBONE RECEPTOR"] = main_binana_out['hbonds'].get("HDONOR_RECEPTOR_BACKBONE_OTHER")
+
+    binana_features["SB ALPHA"] = main_binana_out['salt_bridges'].get("SALT-BRIDGE_ALPHA")
+    binana_features["SB BETA"] = main_binana_out['salt_bridges'].get("SALT-BRIDGE_BETA")
+    binana_features["SB OTHER"] = main_binana_out['salt_bridges'].get("SALT-BRIDGE_OTHER")
+
+    binana_features["piStack ALPHA"] = main_binana_out['salt_bridges'].get("STACKING ALPHA")
+    binana_features["piStack BETA"] = pistacks.get("STACKING BETA")
+    binana_features["piStack OTHER"] = pistacks.get("STACKING OTHER")
+    binana_features["tStack ALPHA"] = pistacks.get("T-SHAPED_ALPHA")
+    binana_features["tStack BETA"] = pistacks.get("T-SHAPED_BETA")
+    binana_features["tStack OTHER"] = pistacks.get("T-SHAPED_OTHER")
+
+    binana_features["catPi BETA LIGAND"] = main_binana_out['pi_cation'].get("PI-CATION_LIGAND-CHARGED_BETA")
+    binana_features["catPi OTHER LIGAND"] = cation_pis.get("PI-CATION_LIGAND-CHARGED_OTHER")
+
+    binana_features["nRot"] = main_binana_out['nrot']
+
+    return binana_features
     return output
 
 def kier_flexibility(lig):
